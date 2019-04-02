697e55751194184e92e1dd7fc5f7b63355e016a5
Pablo Andrade <46230565+pablo-vieira-59@users.noreply.github.com>
2019-04-02 20:02:33 -0300

    Update modelo_ia.py

--- modelo_ia.py
+++ modelo_ia.py
@@ -5,6 +5,8 @@ from keras.models import Sequential
 from keras.layers import Dense
 from keras.models import load_model
 from keras.utils import np_utils
+from keras.layers import Dropout
+from keras.regularizers import l2
 from sklearn.preprocessing import LabelEncoder
 
 # Extrai Respostas dos dados de Treinamento
@@ -22,8 +24,7 @@ def extract_data(all_data):
   subjects_data = data_set[:, 0:2].astype('int')
   return subjects_data
 
-
-data_frame = pd.read_csv('Perfil_Direito.csv')
+data_frame = pd.read_csv('Perfis.csv')
 data_set = data_frame.get_values()
 subjects_data = extract_data(data_set)
 ans_data = extract_answers(data_set)
@@ -33,20 +34,30 @@ encoded = encoder.fit_transform(ans_data)
 
 # Crição do Modelo
 model = Sequential()
-model.add(Dense(6, input_dim=2, activation='relu'))
-model.add(Dense(6, activation='softmax'))
-model.add(Dense(6, activation='softmax'))
+model.add(Dense(10, input_dim=2, activation='tanh'))
+#model.add(Dropout(0.2))
+model.add(Dense(10, activation='tanh'))
+#model.add(Dropout(0.2))
+model.add(Dense(6, activation='sigmoid'))
 
 # Compilação do Modelo
-model.compile(optimizer='sgd', loss='categorical_crossentropy', metrics=['acc'])
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['acc'])
 
 # Treinamento do Modelo
 x = subjects_data
 y = np_utils.to_categorical(encoded)
-model.fit(x, y, epochs=10000)
+
+model.fit(x, y, epochs=1000, batch_size=70, verbose=1)
 
 #Salvando Modelo Treinado
 model.save('modelo.h5')
+
+# Carregando Modelo Treinado
+#model = load_model('modelo.h5')
+
+
+materias = pd.read_csv('Materias.csv')
+materias = materias.get_values()
 
 while 1==1:
   dados = []
@@ -56,4 +67,5 @@ while 1==1:
    dados = [cod_curso,cod_mat]
    dados = np.asmatrix(dados)
    nota = model.predict_classes(dados)
-   print('Codigo Materia : ' + str(cod_mat) + ' - Nota : ' + str(nota[0]))
+   nom_mat = materias[i,0]
+   print('Codigo Materia : ' + nom_mat + ' - Nota : ' + str(nota[0]))
